d4b85802ab3694414341dde7171e42196aafb4a2
marina.oleinik <marina.oleinik@HP-CZC2349HSZ>
2025-09-18 13:23:20 +0300

    1-3 osad OOP

---
+++ Inimene.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TARpv24_Ckeel
+{
+    public abstract class Inimene
+    {
+        public string Nimi;
+        public int Vanus;
+        public Inimene()
+        {
+            //Nimi = "Mati";
+            //Vanus = 99;
+        }
+        public Inimene(string nimi, int vanus)
+        {
+            Nimi = nimi;
+            Vanus = vanus;
+        }
+        public void Tervita()
+        {
+            Console.WriteLine("Tere! Mina olen " + Nimi);
+        }
+        public abstract void Mida_teeb();
+    }
+   
+}
---
+++ StartClassOOP.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TARpv24_Ckeel
+{
+    public class StartClassOOP
+    {
+        public static void Main(string[] args)
+        {
+            Inimene inimene1 = new Inimene();
+            inimene1.Nimi = "Juku";
+            inimene1.Vanus = 12;
+            inimene1.Tervita();
+
+            Inimene inimene2 = new Inimene("Kati", 28);
+            
+            Töötaja töötaja1 = new Töötaja();
+            töötaja1.Nimi = "Mati";
+            töötaja1.Vanus = 45;
+            töötaja1.Ametikoht = "Autojuht";
+            töötaja1.Tervita();
+            töötaja1.Töötan();
+            //3- Tee ise vähemalt 2 alamklassi ja kasuta neid siin
+            //4.5- Tee enda põhiklass ja 2 alamklassi ning kasuta neid siin
+        }
+
+    }
+}
